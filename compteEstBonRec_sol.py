147236b6c1d8be979a5e2fc0c039e293cbd7c2ac
Baptiste <baptiste.baudry@moove-si.com>
2025-01-29 14:29:37 +0100

    final

--- compteEstBonRec_sol.py
+++ compteEstBonRec_sol.py
@@ -3,14 +3,14 @@ import random
 cpt=0
 cache_uses = 0
 
-def memoise(f):
+def memoise(f): # fonction qui permet de mémoriser les résultats des appels à la fonction f
     cache = {}
     def wrapper(cible, nombres):
         global cache_uses
         key = (cible, tuple(sorted(nombres)))
         if key in cache:
             cache_uses += 1
-            #return cache[key]
+            return cache[key]
         result = f(cible, nombres)
         cache[key] = result
         return result
@@ -32,17 +32,9 @@ def trouveEnbrpr(cible, nombres) :
             for nbr in nombres :
                 nombres_temp = nombres[:] # copie de la liste des nombres
                 nombres_temp.remove(nbr) # on enlève nbr de la liste des nombres
-
+                
                 (t, ch) = trouveEnbrpr(cible+nbr, nombres_temp)
                 if t: return (t, ch + " - " + str(nbr))
-
-                if (cible >= nbr) : 
-                    (t, ch) = trouveEnbrpr(cible-nbr, nombres_temp)
-                    if t : return (t, str(nbr)+" + (" + ch+") ")
-
-                if (cible <= nbr) : 
-                    (t, ch) = trouveEnbrpr(nbr-cible, nombres_temp)
-                    if t : return (t, str(nbr)+" + (" + ch+") ")
                 
                 if (cible >= nbr) and cible%nbr == 0: 
                     (t, ch) = trouveEnbrpr(cible//nbr, nombres_temp)
@@ -51,6 +43,10 @@ def trouveEnbrpr(cible, nombres) :
                 if (cible <= nbr) and nbr%cible == 0: 
                     (t, ch) = trouveEnbrpr(nbr//cible, nombres_temp)
                     if t : return (t, str(nbr)+" / ("+ch+") ")
+
+                if (cible >= nbr) : 
+                    (t, ch) = trouveEnbrpr(cible-nbr, nombres_temp)
+                    if t : return (t, str(nbr)+" + (" + ch+") ")
 
                 (t, ch) = trouveEnbrpr(cible*nbr, nombres_temp)
                 if t : return  (t, "("+ch+") / "+str(nbr))
@@ -61,8 +57,8 @@ NBNOMBRES = 6
 nombres=[]
 operateurs = ['+', '-', '*', '/']
 #operandes=list(range(1,11))+list(range(1,11))+[25,50,75,100] # 1 à 10 en double, 25,50,75,100
-cible=799
-nombres =  [4, 8, 5, 6, 6, 2]
+cible=799 
+nombres = [4, 8, 5, 6, 6, 2]
 #for i in range(NBNOMBRES) :
     #nombres.append(operandes[random.randint(0,len(operandes)-1)]) # on tire un nombre au hasard parmit ceunbr possibles
 #cible = random.randint(100,999) # détermine la cible
